dac5516f1f42bd9c8adb8eff92d02d4ff91c37ad
Seth Wheeler <wheelersc@cox.net>
2018-05-25 14:14:19 -0700

    Update main.js

--- UI/JS/main.js
+++ UI/JS/main.js
@@ -9,6 +9,30 @@ $.ajax({
     if(!data)
       alert("NO ITEMINFO");
     else{
+      if (data.Response[1] == "text")
+      {
+       $("#send").prop('disabled', false); 
+       $("#text").prop('disabled', false); 
+       $("#num").prop('disabled', true); 
+      }
+      else if (data.Response[1] == "boolean")
+      {
+       $("#messages").append("<li id='userMessages'>" + 
+       for(var i = 2; i < data.length; i++) 
+        { 
+          + "<input id='" + data.Response[i] +"' type='button' value='" + data.Response[i] +"'/>" + 
+        } 
+       + "</li>"); 
+       $("#send").prop('disabled', true);
+       $("#num").prop('disabled', true);
+       $("#text").prop('disabled', true); 
+      }
+      else if (data.Response[1] == "number")
+      {
+       $("#num").prop('disabled', false);
+       $("#text").prop('disabled', true); 
+       $("#send").prop('disabled', true); 
+      }
       $("#messages").append("<li id='botMessages'>" + data.Response[0] + "</li>");
     }
   } ,
